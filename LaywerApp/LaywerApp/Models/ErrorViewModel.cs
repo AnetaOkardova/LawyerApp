d3d20c8204f9fb8c5c212862ff77aece29729b84
Aneta O <1.aneta.okardova@gmail.com>
2021-03-27 16:00:08 +0100

    Creating project

---
+++ LaywerApp/LaywerApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LaywerApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
